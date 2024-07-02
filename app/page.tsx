1aa98a3fd32596fe82be5a1644f9e81b829106b4
iamShafi <67453945+IamShafi@users.noreply.github.com>
2024-07-03 01:45:53 +0600

    ✨  Designed and coded Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,10 @@
 import Image from "next/image";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main className="mx-auto flex items-center justify-center px-4 py-4 text-white">
-      <div className="flex items-center justify-between flex-1 max-w-[1280px]">
-        <div className="w-48px h-48px">Logo</div>
-        <nav aria-label="Main Navigation">
-          <ul className="flex flex-end gap-[30px] flex-row">
-            <li>Home</li>
-            <li>about</li>
-            <li className="ml-[80px]">Login</li>
-          </ul>
-        </nav>
-      </div>
+    <main className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <Header/>
     </main>
   );
 }
